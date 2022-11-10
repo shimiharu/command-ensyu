b757c693ef25b6e5238862af85f30875df39d6a1
shimiharu <h.shimizu.sys22@morijyobi.ac.jp>
2022-11-10 13:01:34 +0900

    bmiの作成

--- bmi.java
+++ bmi.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-  public class Main {  
+  public class bmi {  
   public static void main(String[] args) {
   Scanner scanner = new Scanner(System.in);
   for(int i = 0; i<2; i++){
