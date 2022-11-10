80df7beb6e5d2898f4350165a00ff87ad6359a80
shimiharu <h.shimizu.sys22@morijyobi.ac.jp>
2022-11-10 12:51:55 +0900

    コミットメッセージを修正

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
   public class Main {  
   public static void main(String[] args) {
   Scanner scanner = new Scanner(System.in);
+  for(int i = 0; i<2; i++){
   System.out.print("身長(cm)=");
   double height = scanner.nextDouble() / 100;
   System.out.print("体重(kg)=");
@@ -12,5 +13,6 @@ import java.util.Scanner;
   String sv = String.format("%.2f", v1);
   double v2 = Double.parseDouble(sv);
   System.out.println("BMIを小数点第二位に表すと="+v2);
+  }
   }
 }
